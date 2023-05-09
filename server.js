cd12280e4629adf5d152a838bf7e6c3bbd7cc958
JA54312 <ija54312@gmail.com>
2023-05-09 13:30:07 -0600

    routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const router = express.Router()
 
 var app = express();
 
-app.use('/',function(req,res){
-    res.send('Holi soy un server en express')
+// app.use('/',function(req,res){
+//     res.send('Holi soy un server en express')
+// })
+
+app.use(router);
+
+router.get('/', function(req,res){
+    res.send('hola desde get')
+})
+
+router.post('/message', function(req,res){
+    res.send('mensaje añadido')
 })
 
 app.listen(3500)
