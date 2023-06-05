e021c10a59b01b8703181ca973f812692967998b
JA54312 <ija54312@gmail.com>
2023-06-05 12:02:15 -0600

    errores

--- network/response.js
+++ network/response.js
@@ -1,7 +1,11 @@
-exports.success = function (req,res,messaage) {
-    res.send(messaage)
+exports.success = function (req,res,message) {
+    res.send(message)
 }
 
-exports.error = function (req,res) {
-    //
+exports.error = function (req,res,message,status,details) {
+    console.error('response error',details)
+    res.status(status || 500).send({
+        error:message,
+        body:""
+    })
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 
-const respones = require('./network/response')
+const response = require('./network/response')
 const router = express.Router()
 
 var app = express();
@@ -20,11 +20,16 @@ router.get('/message', function(req,res){
     })
     //console.log(req.headers)
     //res.send('hola desde get')
-    respones.success(req,res,'Lista de mensajes')
+    response.success(req,res,'Lista de mensajes')
 })
 
 router.post('/message', function(req,res){
     console.log(req.query)
+    if( req.query.error === 'ok'){
+        response.error(req,res,'Error inesperado',500,'Es solo una simulacion de los errores')
+    } else{
+        response.success(req,res,'Creado correctamente,201')
+    }
     console.log(req.body)
     res.send('mensaje añadido correctamente')
 })
