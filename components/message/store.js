44600071f8cc3ee730439dcb7e0c9fc5bf96c627
JA54312 <ija54312@gmail.com>
2023-06-29 12:31:25 -0600

    conection to mongoDB

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,23 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model')
+
+const urlMongo = 'mongodb+srv://ija54312:NsT9IXvSJpCfuOyS@cluster0.xiczhs4.mongodb.net/Telegram'
+db.Promise = global.Promise
+db.connect(urlMongo,{
+    useNewUrlParser:true
+})
+console.log('db conectada con exito')
 
 function addMesasge(message) {
-    list.push(message)
+    //list.push(message)
+    const myMessage = new Model(message);
+    myMessage.save()
 }
 
-function getMessage() {
-    return list;
+async function getMessage() {
+    //return list;
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports= {
