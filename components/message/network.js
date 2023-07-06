2a9139ce6068dd2f0d14da6e27f837e8a61dfdb3
JA54312 <ija54312@gmail.com>
2023-07-06 17:15:14 -0600

    delete

--- components/message/network.js
+++ components/message/network.js
@@ -35,10 +35,14 @@ router.patch('/:id',function(req,res){
         })
 })
 
-router.delete('/', function(req,res){
-    console.log(req.query)
-    console.log(req.body)
-    res.status(201).send({error:'',body:'creado correctamente'})
+router.delete('/:id', function(req,res){
+  controller.deleteMessage(req.params.id)
+  .then(()=>{
+    response.success(req,res,`Mensaje ${req.params.id} eliminado`,200)
+  })
+  .catch(e =>{
+    response.error(req,res, 'Error interno',500,e)
+  })
 })
 
 module.exports = router;
--- components/message/store.js
+++ components/message/store.js
@@ -33,8 +33,15 @@ async function updateText(id,message) {
     return newMessage
 }
 
+function removeMessage(id) {
+    return Model.deleteOne({
+        _id: id
+    })
+}
+
 module.exports= {
     add: addMesasge,
     list: getMessage,
-    updateText: updateText
+    updateText: updateText,
+    remove: removeMessage
 }
