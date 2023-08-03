14bfd8aa97de369cb5636064a5909ab8f7856280
JA54312 <ija54312@gmail.com>
2023-08-03 13:10:43 -0600

    cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app)
 
-
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('./socket')
 const db = require('./db');
@@ -10,6 +10,7 @@ const router = require('./network/routes');
 
 db('mongodb+srv://ija54312:NsT9IXvSJpCfuOyS@cluster0.xiczhs4.mongodb.net/Telegram');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
